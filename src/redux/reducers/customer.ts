373e567e9fd070f0210e38349b723369c965ae6c
John <dropp.boxx@mail.ru>
2020-11-05 21:58:43 +0300

    fix

--- src/redux/reducers/customer.ts
+++ src/redux/reducers/customer.ts
@@ -10,6 +10,12 @@ const initialState: ICustomer[] = [
 export const CustomerReducer = handleActions<ICustomer[], ICustomer>({
     [CustomerActions.Type.ADD_CUSTOMER]: (state, action) => {
      
+        let url = 'http://localhost:4000/weatherforecast'
+
+        let res = fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data))
+
         return [...state, action.payload]
     }
 }, initialState);
